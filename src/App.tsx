57f54dabe70044daa05d940e85e4ff84f8efe986
Abubo Adrian <abubo_a@f112.ub.lan>
2026-01-19 10:16:41 +0800

    Initial React Project

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Test from './Test'
+import Header from './Header'
+import Footer from './Footer'
+
+function App() {
+  
+
+  return (
+    <>
+    <Header/>
+    <Test/>
+    <Footer/>
+    </>
+  )
+}
+
+export default App
